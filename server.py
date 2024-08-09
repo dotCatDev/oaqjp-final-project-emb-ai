272f236259d6d745751d0f840bdfc9d4416f9ca5
Sebastian <sebastian.quinones.inf@gmail.com>
2024-08-09 18:48:24 -0400

    4th Update

--- server.py
+++ server.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request
-from .emotion_detection import emotion_detector
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
-def emotion_detector():
+def sent_detector():
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
-    # Pass the text to the sentiment_analyzer function and store the response
-    response = sentiment_analyzer(text_to_analyze)
+    # Pass the text to the emotion_detector function and store the response
+    response = emotion_detector(text_to_analyze)
 
     # Extract the label and score from the response
     dominant_emotion = response['dominant_emotion']
