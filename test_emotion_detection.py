fa377b7c58aad4b4d6aa3764291da06d008d2e2e
Sebastian <sebastian.quinones.inf@gmail.com>
2024-08-09 18:30:24 -0400

    Third Update

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -5,16 +5,23 @@ import unittest
 class TestEmotionDetection(unittest.TestCase):
     def test_emotion_detection(self):
         # Test case for positive sentiment
-        result_1 = emotion_detector('I am glad this happened')
-        print(result_1['dominant_emotion'])
-        #self.assertEqual(result_1['label'], 'SENT_POSITIVE')
-        
-        # Test case for negative sentiment
-        #result_2 = emotion_detector('I hate working with Python')
-        #self.assertEqual(result_2['label'], 'SENT_NEGATIVE')
-        
-        # Test case for neutral sentiment
-        #result_3 = emotion_detector('I am neutral on Python')
-        #self.assertEqual(result_3['label'], 'SENT_NEUTRAL')
+        emotion = 'I am glad this happened'
+        result_1 = emotion_detector(emotion)
+        print('Statement: ' + emotion + ' domiant emotion: ' + result_1['dominant_emotion'])
+       
+        emotion_2 = 'I am really mad about this'
+        result_2 = emotion_detector(emotion_2)
+        print('Statement: ' + emotion_2 + ' domiant emotion: ' + result_2['dominant_emotion'])
 
+        emotion_3 = 'I feel disgusted just hearing about this'
+        result_3 = emotion_detector(emotion_3)
+        print('Statement: ' + emotion_3 + ' domiant emotion: ' + result_3['dominant_emotion'])
+
+        emotion_4 = 'I am so sad about this'
+        result_4 = emotion_detector(emotion_4)
+        print('Statement: ' + emotion_4 + ' domiant emotion: ' + result_4['dominant_emotion'])
+
+        emotion_5 = 'I am really afraid that this will happen'
+        result_5 = emotion_detector(emotion_5)
+        print('Statement: ' + emotion_5 + ' domiant emotion: ' + result_5['dominant_emotion'])
 unittest.main()
