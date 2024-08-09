8aa7c5a30f66e45461f9c74b2d169a394beed990
Sebastian <sebastian.quinones.inf@gmail.com>
2024-08-09 18:17:48 -0400

    Third Update

---
+++ test_emotion_detection.py
@@ -0,0 +1,20 @@
+from EmotionDetection.emotion_detection import emotion_detector
+import unittest
+
+
+class TestSentimentAnalyzer(unittest.TestCase):
+    def test_sentiment_analyzer(self):
+        # Test case for positive sentiment
+        result_1 = emotion_detector('I am glad this happened')
+        print(result_1['dominant_emotion'])
+        #self.assertEqual(result_1['label'], 'SENT_POSITIVE')
+        
+        # Test case for negative sentiment
+        result_2 = emotion_detector('I hate working with Python')
+        #self.assertEqual(result_2['label'], 'SENT_NEGATIVE')
+        
+        # Test case for neutral sentiment
+        result_3 = emotion_detector('I am neutral on Python')
+        #self.assertEqual(result_3['label'], 'SENT_NEUTRAL')
+
+unittest.main()
